bceaee316ed5056e4ee69ba24c85b8aad459716e
mrfirki <mrfikri94@gmail.com>
2017-04-03 15:32:37 +0800

    added mailer with sidekiq and redis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,5 +53,24 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  #mailer setting starts
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:                'smtp.gmail.com',
+    port:                   587,
+    # domain:               'localhost:3000',
+    user_name:              ENV["GMAIL_USERNAME"],
+    password:               ENV["GMAIL_PASSWORD"],
+    authentication:         'plain',
+    enable_starttls_auto:   true
+
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'mrfikri94@gmail.com'}
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+#mailer setting ends
   
 end
